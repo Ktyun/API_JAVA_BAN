baf2a41c5f0710c864918c483ea788292f20b4f5
Ktyun <kvb5504@naver.com>
2023-06-15 17:15:49 +0900

    구구단 2단 완성 kty

--- Gugudan/src/ezen/Gugudan.java
+++ Gugudan/src/ezen/Gugudan.java
@@ -5,10 +5,14 @@ public class Gugudan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int dan = 2;
+		
 		System.out.println("구구단 연습문제");
 		
-		System.out.println(2 * 1);
-		System.out.println(2 * 9);
+		for(int i = 1; i < 10; i++) {
+			System.out.println(dan + " * " + i + " = " + dan*i);
+		}
+		
 		
 	}
 
